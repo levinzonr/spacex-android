8bced8d0f66f4f5b776dd5e5010737fccbaf9e37
Roman Levinzon <levinzon.roman@gmail.com>
2020-10-06 20:51:55 +0200

    Added some ui elements to the details screen

--- presentation/src/main/java/cz/levinzonr/spotistats/presentation/screens/main/launches/detail/SpaceXLaunchDetailFragment.kt
+++ presentation/src/main/java/cz/levinzonr/spotistats/presentation/screens/main/launches/detail/SpaceXLaunchDetailFragment.kt
@@ -1,7 +1,11 @@
 package cz.levinzonr.spotistats.presentation.screens.main.launches.detail
 
+import androidx.core.view.isVisible
+import coil.load
+import cz.levinzonr.spotistats.domain.models.SpaceXLaunch
 import cz.levinzonr.spotistats.presentation.R
 import cz.levinzonr.spotistats.presentation.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_space_x_launch_detail.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -11,6 +15,13 @@ class SpaceXLaunchDetailFragment : BaseFragment<State>() {
     override val layoutRes: Int = R.layout.fragment_space_x_launch_detail
 
     override fun renderState(state: State) {
+        state.launch?.bind()
+        progressBar.isVisible = state.isLoading
+    }
 
+    private fun SpaceXLaunch.bind() {
+        launchDetailsDescriptionTv.text = details
+        launchDetailsNameTv.text = name
+        launchDetailsIv.load(imagesUrls.firstOrNull())
     }
 }
